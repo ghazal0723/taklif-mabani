7530d5dcfea138fe5cae17f5e147d73a401f982c
ghazal0723 <148717594+ghazal0723@users.noreply.github.com>
2024-03-04 19:29:30 +0330

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int f1 = 0, f2 = 1, f3 = 0, i = 1;
+            int num =int.Parse(textBox1.Text);
+            while (i < num)
+            {
+                f1 = f2;
+                f2 = f3;
+                f3 = f1 + f2;
+                i++;
+            }
+            MessageBox.Show(f3.ToString());
+            
+        }
+    }
+}
